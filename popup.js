95bdd869c2d1e5c268a75ccc340e359994c1eb58
jessechan23 <jessechan10@gmail.com>
2021-06-26 16:47:22 +0100

    Timer added but currently buggy

--- popup.js
+++ popup.js
@@ -1,4 +1,35 @@
-// Initialize button with user's preferred color
+let countdownTimeStart = document.getElementById("countdownTimeStart");
+countdownTimeStart.addEventListener("click", async () => {
+
+var countDownDate = new Date("Jul 26, 2021 17:00:00").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+    // Get todays date and time
+    var now = new Date().getTime();
+
+    // Find the distance between now an the count down date
+    var distance = countDownDate - now;
+
+    // Time calculations for days, hours, minutes and seconds
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    // Output the result in an element with id="demo"
+    document.getElementById("demo").innerHTML = hours + "h "
+    + minutes + "m " + seconds + "s ";
+
+    // If the count down is over, write some text
+    if (distance < 0) {
+        clearInterval(x);
+        document.getElementById("demo").innerHTML = "EXPIRED";
+    }
+}, 1000);
+
+  });
+
 let changeColor = document.getElementById("changeColor");
 
 chrome.storage.sync.get("color", ({ color }) => {
@@ -8,13 +39,13 @@ chrome.storage.sync.get("color", ({ color }) => {
 // When the button is clicked, inject setPageBackgroundColor into current page
 changeColor.addEventListener("click", async () => {
     let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-  
+
     chrome.scripting.executeScript({
       target: { tabId: tab.id },
       function: setPageBackgroundColor,
     });
   });
-  
+
   // The body of this function will be executed as a content script inside the
   // current page
   function setPageBackgroundColor() {
