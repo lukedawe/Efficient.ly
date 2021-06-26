795093f64ecfd27439516aaa88af6a966edf9b9d
jessechan23 <jessechan10@gmail.com>
2021-06-26 17:09:11 +0100

    Fixed errors

--- popup.js
+++ popup.js
@@ -4,7 +4,7 @@
 let countdownTimeStart = document.getElementById("countdownTimeStart");
 countdownTimeStart.addEventListener("click", async () => {
 
-var countDownDate = new Date("Jul 26, 2021 17:00:00").getTime();
+var countDownDate = new Date("Jul 26, 2021 17:07:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
@@ -32,36 +32,13 @@ var x = setInterval(function() {
 }, 1000);
 
   });
-let changeColor = document.getElementById("changeColor");
 
 window.addEventListener("DOMContentLoaded", () => {
   chrome.storage.local.get(["blocked", "enabled"], function (local) {
     const { blocked, enabled } = local;
     if (Array.isArray(blocked)) {
       textarea.value = blocked.join("\n");
-      checkbox.checked = enabled;
+    //  checkbox.checked = enabled;
     }
   });
 });
-
-chrome.storage.sync.get("color", ({ color }) => {
-  changeColor.style.backgroundColor = color;
-});
-
-// When the button is clicked, inject setPageBackgroundColor into current page
-changeColor.addEventListener("click", async () => {
-    let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-
-    chrome.scripting.executeScript({
-      target: { tabId: tab.id },
-      function: setPageBackgroundColor,
-    });
-  });
-
-  // The body of this function will be executed as a content script inside the
-  // current page
-  function setPageBackgroundColor() {
-    chrome.storage.sync.get("color", ({ color }) => {
-      document.body.style.backgroundColor = color;
-    });
-  }
